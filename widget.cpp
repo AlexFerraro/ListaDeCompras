cd5dbdc9111fe5d4fe548e86b99dcdcac8a38fa0
Alex Ferraro <alex__ferraro@hotmail.com>
2020-12-16 23:58:56 -0300

    Version 1.0.0

--- widget.cpp
+++ widget.cpp
@@ -8,6 +8,7 @@ Widget::Widget(QWidget *parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    ui->ProductAddLineEdit->setStyleSheet("QLineEdit { border : 1px solid #79d1ff;}");
 }
 
 Widget::~Widget()
@@ -17,16 +18,18 @@ Widget::~Widget()
 
 void Widget::on_ProductADDpushButton_clicked()
 {
-    QString produto = ui->ProductAddLineEdit->text();
+    QString produto = ui->ProductAddLineEdit->text().trimmed();
 
-
-    if(ui->ProductListWidget->findItems(produto, Qt::MatchExactly).isEmpty())
+    if(ui->ProductListWidget->findItems(produto, Qt::MatchExactly).isEmpty() && produto != "")
     {
-        ui->ProductListWidget->addItem(produto);
+        ui->ProductListWidget->addItem("   " + produto);
+        ui->ProductAddLineEdit->setStyleSheet("QLineEdit { border : 1px solid #79d1ff;}");
         ui->ProductAddLineEdit->clear();
-    }else
+
+    }
+    else
     {
-
-
+        ui->ProductAddLineEdit->setStyleSheet("QLineEdit { border : 1px solid red;}");
+        ui->ProductAddLineEdit->setFocus();
     }
 }
