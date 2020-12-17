14f2c8670ae87d9e4457bb7ef9dfc39bf6127a49
Alex Ferraro <alex__ferraro@hotmail.com>
2020-12-16 22:47:56 -0300

    Version 1.0.0

--- widget.cpp
+++ widget.cpp
@@ -1,6 +1,8 @@
 #include "widget.h"
 #include "ui_widget.h"
 
+using namespace std;
+
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
@@ -13,3 +15,18 @@ Widget::~Widget()
     delete ui;
 }
 
+void Widget::on_ProductADDpushButton_clicked()
+{
+    QString produto = ui->ProductAddLineEdit->text();
+
+
+    if(ui->ProductListWidget->findItems(produto, Qt::MatchExactly).isEmpty())
+    {
+        ui->ProductListWidget->addItem(produto);
+        ui->ProductAddLineEdit->clear();
+    }else
+    {
+
+
+    }
+}
--- widget.h
+++ widget.h
@@ -15,6 +15,9 @@ public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
 
+private slots:
+    void on_ProductADDpushButton_clicked();
+
 private:
     Ui::Widget *ui;
 };
